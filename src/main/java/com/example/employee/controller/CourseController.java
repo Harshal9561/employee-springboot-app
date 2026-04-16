266145d59b5fd21b62229ea12ce52c3785c554dd
Harshal <harshal.kadam@nobrainsolutions.com>
2026-04-16 23:41:39 +0530

    Initial Spring Boot employee project

--- src/main/java/com/example/employee/controller/CourseController.java
+++ src/main/java/com/example/employee/controller/CourseController.java
@@ -2,30 +2,14 @@ package com.example.employee.controller;
 
 
 
-import com.example.employee.entity.Course;
-import com.example.employee.service.CourseService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/courses")
 @RequiredArgsConstructor
 public class CourseController {
 
-    @Autowired
-    private CourseService courseService;
-
-    @PostMapping(consumes = "multipart/form-data")
-    public Course createCourse(
-            @RequestParam String courseName,
-            @RequestParam String duration,
-            @RequestParam Double price,
-            @RequestParam MultipartFile image
-    ) throws Exception {
-        return courseService.saveCourse(courseName, duration, price, image);
-    }
 
     // ✅ Dummy test endpoint
     @GetMapping("/test")
